b2b3e7daf54a52a0e0295b25b55665d2df8ce6ad
Still1 <oliverstill1@gmail.com>
2017-07-25 00:10:13 +0800

    Add unit test.
    Signed-off-by: Still1 <oliverstill1@gmail.com>

--- oc-core/src/test/java/com/oc/ocframework/core/spring/controller/RootControllerTest.java
+++ oc-core/src/test/java/com/oc/ocframework/core/spring/controller/RootControllerTest.java
@@ -5,7 +5,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
+/**
+ *	Spring MVC 应用根目录Controller测试类
+ */
 public class RootControllerTest {
 	@Test
 	public void testRoot() throws Exception {
